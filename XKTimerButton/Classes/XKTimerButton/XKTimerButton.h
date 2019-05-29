dd5d217363733bc9f5fc91a48bf4784a98549daa
xieweikun <kunhum@163.com>
2019-05-29 20:26:59 +0800

    Release 1.0.4

--- XKTimerButton/Classes/XKTimerButton/XKTimerButton.h
+++ XKTimerButton/Classes/XKTimerButton/XKTimerButton.h
@@ -13,7 +13,7 @@
 ///开始倒计时
 - (void)xk_startCounting;
 
-@property (nonatomic, copy) void(^xkTimerCounting)(NSInteger leftInterval);
+@property (nonatomic, copy) void(^xkTimerCounting)(NSInteger leftInterval, XKTimerButton *button);
 @property (nonatomic, copy) BOOL(^xkStartCounting)(XKTimerButton *button);
 
 ///默认60s
@@ -23,7 +23,13 @@
 @property (nonatomic, copy) NSString *countingPrefix;
 ///倒数时的后缀,默认为 @""
 @property (nonatomic, copy) NSString *countingSuffix;
-///倒计时状态下的颜色 默认为313131 50%
-@property (nonatomic, strong) UIColor *countingColor;
+///倒计时状态下文字的颜色 默认为313131 50%
+@property (nonatomic, strong) UIColor *countingTextColor;
+///倒计时状态下背景的颜色 默认为dedede
+@property (nonatomic, strong) UIColor *countingBackgroundColor;
+///倒计时状态下文字的颜色 默认为一开始文字
+@property (nonatomic, strong) UIColor *normalTextColor;
+///倒计时状态下背景的颜色 默认为一开始背景颜色
+@property (nonatomic, strong) UIColor *normalBackgroundColor;
 
 @end
--- XKTimerButton/Classes/XKTimerButton/XKTimerButton.m
+++ XKTimerButton/Classes/XKTimerButton/XKTimerButton.m
@@ -44,10 +44,13 @@
 - (void)initMethod {
     
     [self addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchUpInside];
-    self.countingColor  = [[UIColor colorWithRed:49/255.0 green:49/255.0 blue:49/255.0 alpha:1.0] colorWithAlphaComponent:0.5];
-    self.timerInterval  = 60;
-    self.countingPrefix = @"重新获取 ";
-    self.countingSuffix = @"";
+    self.countingTextColor       = [[UIColor colorWithRed:49/255.0 green:49/255.0 blue:49/255.0 alpha:1.0] colorWithAlphaComponent:0.5];
+    self.countingBackgroundColor = [self colorWithHexString:@"dedede"];
+    self.normalTextColor         = self.titleLabel.textColor;
+    self.normalBackgroundColor   = self.backgroundColor;
+    self.timerInterval           = 60;
+    self.countingPrefix          = @"重新获取 ";
+    self.countingSuffix          = @"";
     
     if ([self.currentTitle isEqualToString:@"button"]) {
         [self setTitle:@"获取验证码" forState:UIControlStateNormal];
@@ -65,7 +68,8 @@
     if (canStart) {
         self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerMethod) userInfo:nil repeats:YES];
         [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
-        [self setTitleColor:self.countingColor forState:UIControlStateNormal];
+        [self setTitleColor:self.countingTextColor forState:UIControlStateNormal];
+        self.backgroundColor = self.countingBackgroundColor;
         self.userInteractionEnabled = NO;
     }
 }
@@ -80,16 +84,18 @@
 - (void)timerMethod {
     
     if (--_settingInterval <= 0) {
-        !self.xkTimerCounting ?: self.xkTimerCounting(0);
+        !self.xkTimerCounting ?: self.xkTimerCounting(0, self);
         _settingInterval = self.timerInterval;
         [self.timer invalidate];
         self.timer = nil;
         [self setTitle:@"获取验证码" forState:UIControlStateNormal];
         [self setTitleColor:self.normalColor  forState:UIControlStateNormal];
         self.userInteractionEnabled = YES;
+        [self setTitleColor:self.normalTextColor forState:UIControlStateNormal];
+        self.backgroundColor = self.normalBackgroundColor;
         return;
     }
-    !self.xkTimerCounting ?: self.xkTimerCounting(_settingInterval);
+    !self.xkTimerCounting ?: self.xkTimerCounting(_settingInterval, self);
     [self setTitle:[NSString stringWithFormat:@"%@%02ld%@",self.countingPrefix,_settingInterval,self.countingSuffix] forState:UIControlStateNormal];
 }
 
@@ -110,9 +116,70 @@
     self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerMethod) userInfo:nil repeats:YES];
     [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self setTitleColor:self.countingColor  forState:UIControlStateNormal];
+        [self setTitleColor:self.countingTextColor  forState:UIControlStateNormal];
     });
     self.userInteractionEnabled = NO;
+}
+
+- (UIColor *)colorWithHexString:(NSString *)color {
+    NSString *cString = [[color stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) {
+        return [UIColor clearColor];
+    }
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"])
+        cString = [cString substringFromIndex:2];
+    if ([cString hasPrefix:@"#"])
+        cString = [cString substringFromIndex:1];
+    if ([cString length] != 6)
+        return [UIColor clearColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    
+    //r
+    NSString *rString = [cString substringWithRange:range];
+    
+    //g
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    //b
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f) green:((float) g / 255.0f) blue:((float) b / 255.0f) alpha:1.0f];
+}
+
+- (void)xk_setTimerWithQueue:(dispatch_queue_t)queue seconds:(double)seconds interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway countingHandler:(void (^ _Nullable)(CGFloat))countingHandler cancelHandler:(void (^ _Nullable)(void))cancelHandler {
+    
+    __block double countingSeconds = seconds;
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, interval * NSEC_PER_SEC, leeway * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(timer, ^{
+        if (--countingSeconds > 0) {
+            !countingHandler ?: countingHandler(countingSeconds);
+        }
+        else {
+            dispatch_cancel(timer);
+        }
+    });
+    dispatch_source_set_cancel_handler(timer, ^{
+        !cancelHandler ?: cancelHandler();
+    });
+    dispatch_resume(timer);
+    
 }
 
 - (void)dealloc {
